eccb15dd1cc40b3224ff9001712112b12cfef70d
Mikkel <90240970+mikkelbirkegaard@users.noreply.github.com>
2022-08-26 20:11:10 +0200

    Add files via upload

---
+++ app/controllers/controller.js
@@ -0,0 +1,52 @@
+export default class Controller{
+    initialize(model, view){
+        this.model = model;
+        this.view = view;
+    }
+
+    buildTemplate(guitar){
+        return `<tr class="customerrow">
+        <td>${guitar.getSerialNumber()}</td>
+        <td>${guitar.getBuilder()}</td>
+        <td>${guitar.getModel()}</td>
+        <td>${guitar.getType()}</td>
+        <td>${guitar.getBackwood()}</td>
+        <td>${guitar.getTopwood()}</td>
+        <td>${guitar.getPrice()}</td>
+        </tr>`;
+    }
+
+    snSearch (serialNumber){
+        const guitar = this.model.guitarList.getGuitar(serialNumber);
+        let template = "";
+
+        if (guitar !== null){
+            template = this.buildTemplate(guitar);
+        } else {
+            template = `
+            <tr class ="customerrow">
+            <td colspan="8">Nothing to show</td>
+            </tr>`;
+        }
+        this.view.message(template);
+    }
+
+    search (searchGuitar){
+        const guitar = this.model.guitarList.search(searchGuitar);
+        let template = "";
+
+        if (guitar !== null){
+        template = this.buildTemplate(guitar);
+        } else {
+        template = `
+        <tr class ="customerrow">
+        <td colspan="8">Nothing to show</td>
+        </tr>`;
+    }
+    this.view.message(template);
+} 
+
+}
+
+
+
